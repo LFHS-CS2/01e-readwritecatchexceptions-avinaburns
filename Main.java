9180c4793babea12a6d3e6c05efa17bca2cb90a4
Avina Burns <2150083@lfschools.net>
2020-08-28 18:49:02 +0000

    I think it worked

--- Main.java
+++ Main.java
@@ -2,13 +2,20 @@ import java.io.*;
 import java.util.*;
 class Main {
     public static void main(String[] args)  {
+       try {
        Scanner in = new Scanner(new File("test.in"));
        PrintWriter out = new PrintWriter("test.out");
        while (in.hasNextLine()) {      
-           out.println(in.nextLine());   
+           out.println(in.nextLine()); 
+       } 
+            in.close();   
+            out.close();
+       
+       }
+       catch (FileNotFoundException ex){
+        System.out.println("File wan't found." ); 
        }   
-        in.close();   
-        out.close();
+       
         
     }
 }
